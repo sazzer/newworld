dce946ae9934f48cf2ec5264c09f39965a266f0f
Graham Cox <graham@grahamcox.co.uk>
2018-05-11 18:30:51 +0100

    Set up Jest

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
     "extends": [
         "airbnb-base",
-        "plugin:flowtype/recommended"
+        "plugin:flowtype/recommended",
+        "plugin:jest/recommended",
     ],
     "parser": "babel-eslint",
     "plugins": [
-        "flowtype"
+        "flowtype",
+        "jest",
     ],
-
+    "env": {
+        "jest/globals": true,
+    },
+    "rules": {
+        "indent": ["error", 4],
+    }
 };
---
+++ backend/src/__tests__/simple.test.js
@@ -0,0 +1,3 @@
+test('Jest works', () => {
+    expect(1 + 1).toBe(2);
+});
