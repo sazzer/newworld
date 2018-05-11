62b84e440c0b9e0b303da4e470af5e649beae3c1
Graham Cox <graham@grahamcox.co.uk>
2018-05-11 19:41:11 +0100

    Some config tweaks

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
     verbose: true,
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.js',
+        '!src/index.js',
+        '!src/config/*.js',
+    ],
+    clearMocks: true,
+    roots: [
+        'src',
+    ],
+    notify: true,
 };
