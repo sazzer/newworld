fd385fe64744c48cd9835cd53c5b34018ce00aa0
Graham Cox <graham@grahamcox.co.uk>
2018-05-14 08:02:53 +0100

    Set up Docker Compose to run the backend

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
     verbose: true,
     collectCoverage: true,
@@ -5,10 +7,14 @@ module.exports = {
         'src/**/*.js',
         '!src/index.js',
         '!src/config/*.js',
+        '!src/server/index.js',
     ],
     clearMocks: true,
     roots: [
         'src',
     ],
     notify: true,
+    setupFiles: [
+        path.join(__dirname, 'src', 'testSetup.js'),
+    ],
 };
