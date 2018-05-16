26fa4855560bdada9da9c869ab51639b5595fc6d
Graham Cox <graham@grahamcox.co.uk>
2018-05-16 13:26:05 +0100

    Set up Redux and React Router

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,8 +1,25 @@
+// @flow
+import 'semantic-ui-css/semantic.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
+import { ConnectedRouter as Router } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { store, history } from './redux'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * The wrapper around the main application to set everything up
+ * @return {*} the main application
+ */
+const AppWrapper = () => (
+    <Provider store={store}>
+        <Router history={history}>
+            <App />
+        </Router>
+    </Provider>
+);
+
+ReactDOM.render(<AppWrapper />, document.getElementById('root'));
 registerServiceWorker();
