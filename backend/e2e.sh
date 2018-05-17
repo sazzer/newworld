905f10a27ac657deffcb513c1863b6d0cc01bab7
Graham Cox <graham@grahamcox.co.uk>
2018-05-17 12:43:53 +0100

    Shippable configuration

--- backend/e2e.sh
+++ backend/e2e.sh
@@ -2,4 +2,4 @@
 
 docker-compose -f docker-compose.cucumber.yml down
 docker-compose -f docker-compose.cucumber.yml rm -f
-docker-compose -f docker-compose.cucumber.yml up --build --exit-code-from worlds-e2e-cucumber
+docker-compose -f docker-compose.cucumber.yml up --build --exit-code-from worlds-cucumber-tests
