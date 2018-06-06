68a6ba8819eabbe9cd262454e8f8e31f4488c719
Graham Cox <graham@grahamcox.co.uk>
2018-06-06 17:09:18 +0100

    Stripped out the UI parts that we don't want

--- worlds-webapp/src/App.js
+++ worlds-webapp/src/App.js
@@ -1,21 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-class App extends Component {
-  render() {
+import React from 'react';
+
+export default function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+        </div>
     );
-  }
 }
-
-export default App;
