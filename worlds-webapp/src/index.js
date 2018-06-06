68f87372826eddb41f113f19a5612761a5ed9551
Graham Cox <graham@grahamcox.co.uk>
2018-06-06 18:40:30 +0100

    Set up i18n

--- worlds-webapp/src/index.js
+++ worlds-webapp/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {I18nextProvider, translate} from 'react-i18next';
 import 'semantic-ui-css/semantic.min.css';
+import i18n from './i18n';
 import App from './ui/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * The contents of the app, wrapped in the translations layer
+ */
+const TranslatedAppContents = translate(['worlds'], {wait: true})(App);
+
+
+/**
+ * The wrapper around the main application to set everything up
+ * @return {*} the main application
+ */
+const AppWrapper = () => (
+    <I18nextProvider i18n={ i18n }>
+        <TranslatedAppContents />
+    </I18nextProvider>
+);
+
+ReactDOM.render(<AppWrapper />, document.getElementById('root'));
 registerServiceWorker();
--- worlds-webapp/src/ui/header/HeaderBar.js
+++ worlds-webapp/src/ui/header/HeaderBar.js
@@ -1,15 +1,20 @@
 // @flow
 
 import React from 'react';
+import { Interpolate } from 'react-i18next';
 import { Menu } from 'semantic-ui-react'
 
 export default function HeaderBar() {
     return (
         <Menu fixed="top" inverted borderless>
-            <Menu.Item header>Worlds</Menu.Item>
+            <Menu.Item header>
+                <Interpolate i18nKey="page.title" />
+            </Menu.Item>
 
             <Menu.Menu position="right">
-                <Menu.Item>Log In / Register</Menu.Item>
+                <Menu.Item>
+                    <Interpolate i18nKey="page.login" />
+                </Menu.Item>
             </Menu.Menu>
         </Menu>
     );
