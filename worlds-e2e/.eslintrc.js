809441810c3f069de99a46bf2cc29310909bce72
Graham Cox <graham@grahamcox.co.uk>
2018-06-17 23:24:25 +0100

    Enough to get Cucumber set up and working for the E2E tests, and running inside Docker

---
+++ worlds-e2e/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": [
+        "react-app",
+        "plugin:flowtype/recommended"
+    ],
+    "parser": "babel-eslint",
+    "plugins": [
+        "flowtype"
+    ],
+    "env": {
+    },
+    "rules": {
+    }
+};
