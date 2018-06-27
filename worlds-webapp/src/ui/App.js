b3c27366586dfd603b42e0e6c86cb0f582acbf17
Graham Cox <graham@grahamcox.co.uk>
2018-06-27 13:06:47 +0100

    Component to represent the core of the page

--- worlds-webapp/src/ui/App.js
+++ worlds-webapp/src/ui/App.js
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import HeaderBar from './header';
+import Page from './Page';
 
 export default function App() {
     return (
         <div id="App">
             <HeaderBar />
+            <Page />
         </div>
     );
 }
---
+++ worlds-webapp/src/ui/Page.js
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+/**
+ * The actual main page of the application
+ */
+export default function Page() {
+    return (
+        <Container fluid>
+        </Container>
+    );
+}
